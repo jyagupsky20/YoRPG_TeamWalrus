37dd5fa45048f5bcd599f742dad2d5680fce2ac8
jyagupsky20 <jyagupsky20@stuy.edu>
2021-11-22 22:17:28 -0500

    New and improved file structure

---
+++ Character.java
@@ -0,0 +1,26 @@
+public class Character{
+	int health;
+	int strength;
+	int defense;
+	double attack_rating;
+	public Character(int health, int strength, int defense, double attack_rating){
+		this.health = health;
+		this.strength = strength;
+		this.defense = defense;
+		this.attack_rating = attack_rating;
+	}
+	public boolean isAlive(){
+    return (health > 0);
+  }
+	public int getDefense(){
+		return defense;
+	}
+	public void lowerHP(int amount){
+		health -= amount;
+	}
+	public int attack(Character defendant){
+		int damage = (int) Math.max((this.strength*this.attack_rating)-defendant.getDefense(),0);
+		defendant.lowerHP(damage);
+		return damage;
+	}
+}
---
+++ Monster.java
@@ -0,0 +1,5 @@
+public class Monster extends Character{
+  public Monster() {
+    super(100, 20, 15, 1.0);
+  }
+}
